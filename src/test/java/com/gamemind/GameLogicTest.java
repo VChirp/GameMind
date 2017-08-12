dd39391c9afefd6151871e9cd7e45c5d89ed0210
chirp <farel536@gmail.com>
2017-08-12 13:50:46 +0300

    Adding new tests for GameLogic.userClick

--- src/test/java/com/gamemind/GameLogicTest.java
+++ src/test/java/com/gamemind/GameLogicTest.java
@@ -2,9 +2,13 @@ package com.gamemind;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class GameLogicTest {
+    private Parser parser = new Parser();
+
     /*
    6T|7T|1T|7T
    3T|4T|8T|5F
@@ -12,15 +16,47 @@ public class GameLogicTest {
    1T|5T|8T|2T
      */
     @Test
-    public void testClick() throws Exception {
+    public void testClickTrue() throws Exception {
 //        Given
-//        TODO initialize GameLogic
-        GameLogic gameLogic = new GameLogic(null);
+        GameLogic gameLogic = new GameLogic(parser.parse("6F|5F"));
 
 //        When
         MyPoint[][] myPoints = gameLogic.userClick(0, 0);
 
 //        Then
         assertTrue(myPoints[0][0].isWasSelected());
+    }
+
+    @Test
+    public void testClickValue() throws Exception {
+        GameLogic gameLogic = new GameLogic(parser.parse("6F|5F"));
+
+        MyPoint[][] myPoints = gameLogic.userClick(0, 1);
+
+        assertEquals(myPoints[0][1].getValue(), 5);
+    }
+
+    @Test
+    public void testClickEqualsTrue() throws Exception {
+        GameLogic gameLogic = new GameLogic(parser.parse("5F|5F"));
+
+        MyPoint[][] myPoints;
+        gameLogic.userClick(0,0);
+        myPoints = gameLogic.userClick(0,1);
+
+        assertTrue(myPoints[0][0].isWasSelected());
+        assertTrue(myPoints[0][1].isWasSelected());
+    }
+
+    @Test
+    public void testClickEqualsFalse() throws Exception {
+        GameLogic gameLogic = new GameLogic(parser.parse("5F|2F"));
+
+        MyPoint[][] myPoints;
+        gameLogic.userClick(0,0);
+        myPoints = gameLogic.userClick(0,1);
+
+        assertFalse(myPoints[0][0].isWasSelected());
+        assertFalse(myPoints[0][1].isWasSelected());
     }
 }
